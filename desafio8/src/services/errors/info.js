8abf9811bb0a5955dc3e2cc0e60a4f63de2e9e11
Nahuel Buccafusca <n.buccafusca@outlook.com.ar>
2024-07-23 21:26:38 -0300

    desafio 8

---
+++ desafio8/src/services/errors/info.js
@@ -0,0 +1,13 @@
+const generateUserErrorInfo = (user) => {
+    return `Uno o más de los siguientes campos fueron completados erróneamente.
+     Lista de requerimientos:
+      * first_name : Necesita cadena de texto, se recibió ${user.first_name};
+      * last_name  : Necesita cadena de texto, se recibió ${user.last_name};
+      * email      : Necesita cadena de texto, se recibió ${user.emails}`;
+  };
+  
+  const iqualCode = (code) => {
+    return `Código de producto ${code} existente en la base de datos`;
+  };
+  
+  module.exports = { generateUserErrorInfo, iqualCode };
